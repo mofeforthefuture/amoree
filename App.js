1752eb083fd1f6fbdfd3ea58065be8708be760f6
Omotayo Eyimofe <eyimofeomotayo@gmail.com>
2024-05-25 09:16:08 +0100

    bootstrapping navigation

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
-import {View, Text} from 'react-native';
 import React, {useEffect} from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SplashScreen from 'react-native-splash-screen';
+import {LandingPage} from './src/screens';
+
+const {Navigator, Screen} = createNativeStackNavigator();
 
 export default function App() {
   useEffect(() => {
+    console.log('====================================');
+    console.log('ge');
+    console.log('====================================');
     SplashScreen.hide(); //to ensure the javascript loads completely and avoid the brief white screen
   }, []);
 
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{headerShown: false}}
+        initialRouteName="LandingPage">
+        <Screen name="LandingPage" component={LandingPage} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
---
+++ src/assets/images/index.js
@@ -0,0 +1,3 @@
+const landing = require('./landing.png');
+
+export default {landing};
