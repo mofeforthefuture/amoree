634e07c8635c239418d25d6cd87203d40a4e2eeb
Omotayo Eyimofe <eyimofeomotayo@gmail.com>
2024-05-31 05:58:16 +0100

    config for screens

--- App.js
+++ App.js
@@ -4,21 +4,27 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SplashScreen from 'react-native-splash-screen';
 import {CountryList, LandingPage, Login, Signup} from './src/screens';
-import useInitialURL from './src/hooks/useInitialURL';
 
 const {Navigator, Screen, Group} = createNativeStackNavigator();
 
 export default function App() {
-  const {url: initialUrl, processing} = useInitialURL();
-  console.log('====================================');
-  console.log(initialUrl, processing);
-  console.log('====================================');
   useEffect(() => {
     SplashScreen.hide(); //to ensure the javascript loads completely and avoid the brief white screen
   }, []);
 
+  const config = {
+    screens: {
+      Login: 'login',
+      Signup: 'register',
+    },
+  };
+  const linking = {
+    prefixes: ['amorevista://'],
+    config,
+  };
+
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <Navigator
         screenOptions={{headerShown: false}}
         initialRouteName="LandingPage">
